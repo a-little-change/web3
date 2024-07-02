1d9ecbbdb7a4eb2bab4a6e80e8d318548bed8545
a-little-change <68430480+a-little-change@users.noreply.github.com>
2024-07-02 21:39:34 +0800

    Add files via upload

--- d01_test_pow/test_pow.go
+++ d01_test_pow/test_pow.go
@@ -4,34 +4,25 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"math/rand"
-	"strconv"
 	"strings"
 	"time"
 )
 
-func testPow(name string, difficulty int) {
+func testPow(name string, difficulty int) string {
 	for i := 0; ; i++ {
 		start := time.Now()
-		rand.Seed(time.Now().UnixNano())
-		nonce := strconv.Itoa(rand.Int())
+		nonce := fmt.Sprintf("%d", i)
 		hash := calculateHash(name, nonce)
 		if !isDifficultyValid(hash, difficulty) {
-
-			fmt.Println(hash)
-			fmt.Println(name)
-			fmt.Println(nonce)
-			fmt.Println("失败!")
 			continue
 		} else {
 			end := time.Now()
-
-			fmt.Println("消耗时间：" + end.Sub(start).String() + "ms")
+			fmt.Printf("消耗时间：%d ms\n", end.Sub(start).Milliseconds())
 			fmt.Println(hash)
 			fmt.Println(name)
 			fmt.Println(nonce)
 			fmt.Println("成功")
-			break
+			return hash
 		}
 	}
 }
@@ -47,9 +38,4 @@ func calculateHash(name string, nonce string) string {
 	hash.Write([]byte(record))
 	hashed := hash.Sum(nil)
 	return hex.EncodeToString(hashed)
-}
-
-func main() {
-	testPow("Alex", 4)
-	testPow("Clark", 5)
 }
