00396419ec13659e3faddcb852ff070d161e97ca
a_little_change <xiarj186@gmail.com>
2024-07-13 21:18:10 +0800

    Foundry部署

---
+++ week2_day5/depoly_smart_contract/script/MyToken.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+contract MyTokenScript is Script {
+    MyToken public myToken;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        myToken = new MyToken("PetToken", "Pet");
+        vm.stopBroadcast();
+    }
+}
