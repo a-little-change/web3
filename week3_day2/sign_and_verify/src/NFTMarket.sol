9e2e628e218cbcb4078e2b8f535e5b8e32180660
a_little_change <xiarj186@gmail.com>
2024-07-21 20:21:10 +0800

    修改NFTMarket

--- week3_day2/sign_and_verify/src/NFTMarket.sol
+++ week3_day2/sign_and_verify/src/NFTMarket.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import "./PetToken.sol";
-import "./PetNFT.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC2612} from "@openzeppelin/contracts/interfaces/IERC2612.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
-contract NFTMarket is EIP712 {
+contract NFTMarket is EIP712, Ownable {
     bytes32 private constant PERMIT_TYPEHASH =
         keccak256("CheckWhiteList(address buyer,uint256 deadline)");
-
-    PetToken public tokenContract;
-
-    address immutable owner;
 
     // NFT合约地址 => TokenId => 上架的NFT
     mapping(address => mapping(uint256 => ListedNFT)) listedNFTs;
@@ -20,6 +17,7 @@ contract NFTMarket is EIP712 {
     // 上架的NFT
     struct ListedNFT {
         address seller;
+        address tokenAddr;
         uint256 price;
     }
 
@@ -32,85 +30,57 @@ contract NFTMarket is EIP712 {
         uint8 v;
         bytes32 r;
         bytes32 s;
+        uint256 deadline;
     }
 
-    error IsSelledOrNotListed(address nftAddress, uint tokenId);
-
-    error CannotBuyYouOwnNFT(address nftAddress, uint tokenId, address owner);
-
-    error TransferFailed(address from, address to, uint value);
-
-    error ExpiredSignature(uint256 deadline);
-
-    error InvalidBuyer(address personInWhiteList, address buyer);
-
-    // 上架事件
-    event Listed(
-        address indexed seller,
-        address indexed nftAddress,
-        uint indexed tokenId,
-        uint price
-    );
-
-    // sell success event
-    event NFTSellSuccess(
-        address indexed seller,
-        address indexed buyer,
-        uint tokenId
-    );
-
-    constructor(address tokenAddress, address _owner) EIP712("NFT", "1") {
-        tokenContract = PetToken(tokenAddress);
-        owner = _owner;
-    }
+    constructor() Ownable(msg.sender) EIP712("NFTMarket", "1") {}
 
     function sellerOf(
-        address nftAddress,
+        address nftAddr,
         uint tokenId
     ) public view returns (address) {
-        return listedNFTs[nftAddress][tokenId].seller;
-    }
-    function priceOf(
-        address nftAddress,
-        uint tokenId
-    ) public view returns (uint) {
-        return listedNFTs[nftAddress][tokenId].price;
+        return listedNFTs[nftAddr][tokenId].seller;
     }
 
-    function list(address nftAddress, uint256 tokenId, uint256 price) external {
-        IERC721 nft = IERC721(nftAddress);
-        require(nft.ownerOf(tokenId) == msg.sender, "You are not the Owner!");
-        listedNFTs[nftAddress][tokenId] = ListedNFT({
+    function priceOf(address nftAddr, uint tokenId) public view returns (uint) {
+        return listedNFTs[nftAddr][tokenId].price;
+    }
+
+    function list(
+        address nftAddr,
+        uint tokenId,
+        address tokenAddr,
+        uint price
+    ) external {
+        if (IERC721(nftAddr).ownerOf(tokenId) != msg.sender)
+            revert IsNotTheOwner(IERC721(nftAddr).ownerOf(tokenId), msg.sender);
+        listedNFTs[nftAddr][tokenId] = ListedNFT({
             seller: msg.sender,
+            tokenAddr: tokenAddr,
             price: price
         });
-        emit Listed(msg.sender, nftAddress, tokenId, price);
+        emit Listed(msg.sender, nftAddr, tokenId, tokenAddr, price);
     }
 
-    function buyNFT(address nftAddress, uint256 tokenId) external {
-        IERC721 nft = IERC721(nftAddress);
-        ListedNFT memory listedNFT = listedNFTs[nftAddress][tokenId];
-
+    function buyNFT(address nftAddr, uint256 tokenId) external {
+        IERC721 nft = IERC721(nftAddr);
+        //
+        ListedNFT memory listedNFT = listedNFTs[nftAddr][tokenId];
+        IERC20 token = IERC20(listedNFT.tokenAddr);
         // 判断该NFT是否上架或被卖出
         if (listedNFT.seller == address(0)) {
-            revert IsSelledOrNotListed(nftAddress, tokenId);
+            revert IsSelledOrNotListed(nftAddr, tokenId);
         }
         //
         if (listedNFT.seller == msg.sender) {
-            revert CannotBuyYouOwnNFT(nftAddress, tokenId, msg.sender);
+            revert CannotBuyYouOwnNFT(nftAddr, tokenId, msg.sender);
         }
-        // 判断余额是否充足
-        require(
-            tokenContract.balanceOf(msg.sender) >= listedNFT.price,
-            "You balance isn't enough"
-        );
-        tokenContract.transferFrom(
-            msg.sender,
-            listedNFT.seller,
-            listedNFT.price
-        );
+        // check whether the balance is enough
+        if (token.balanceOf(msg.sender) < listedNFT.price)
+            revert InsufficientBalance(msg.sender);
+        token.transferFrom(msg.sender, listedNFT.seller, listedNFT.price);
         nft.safeTransferFrom(listedNFT.seller, msg.sender, tokenId);
-        delete listedNFTs[nftAddress][tokenId];
+        delete listedNFTs[nftAddr][tokenId];
     }
 
     function hashStruct(
@@ -129,48 +99,53 @@ contract NFTMarket is EIP712 {
     }
 
     function permitBuy(
-        address nftAddress,
+        address nftAddr,
         uint256 tokenId,
-        uint256 deadline,
-        uint256 deadline1,
         SignedData calldata nftData,
         SignedData calldata tokenData
     ) external {
-        IERC721 nft = IERC721(nftAddress);
+        IERC721 nft = IERC721(nftAddr);
         // check whether the buyer is in white list
-        checkWhiteList(msg.sender, deadline, nftData);
-
-        ListedNFT memory listedNFT = listedNFTs[nftAddress][tokenId];
+        checkWhiteList(msg.sender, nftData);
+        //get the listed NFT info
+        ListedNFT memory listedNFT = listedNFTs[nftAddr][tokenId];
+        IERC20 token = IERC20(listedNFT.tokenAddr);
         // 判断该NFT是否上架或被卖出
         if (listedNFT.seller == address(0))
-            revert IsSelledOrNotListed(nftAddress, tokenId);
+            revert IsSelledOrNotListed(nftAddr, tokenId);
         // buyer is the seller
         if (listedNFT.seller == msg.sender)
-            revert CannotBuyYouOwnNFT(nftAddress, tokenId, msg.sender);
-
-        // 判断余额是否充足
-        require(
-            tokenContract.balanceOf(msg.sender) >= listedNFT.price,
-            "You balance isn't enough"
-        );
+            revert CannotBuyYouOwnNFT(nftAddr, tokenId, msg.sender);
+        // whether the buyer has engouth balance
+        if (token.balanceOf(msg.sender) < listedNFT.price)
+            revert InsufficientBalance(msg.sender);
         // get buyer permit
-        tokenContract.permit(
+        IERC2612(listedNFT.tokenAddr).permit(
             msg.sender,
             address(this),
             listedNFT.price,
-            deadline1,
+            tokenData.deadline,
             tokenData.v,
             tokenData.r,
             tokenData.s
         );
         //delete the listed NFT info
-        delete listedNFTs[nftAddress][tokenId];
-        // transfer from buyer to owner
-        bool res = tokenContract.transferFrom(msg.sender, owner, listedNFT.price);
-        if (!res) revert TransferFailed(msg.sender, owner, listedNFT.price);
+        delete listedNFTs[nftAddr][tokenId];
+        // transfer from buyer to seller
+        bool res = token.transferFrom(
+            msg.sender,
+            listedNFT.seller,
+            listedNFT.price
+        );
+        if (!res)
+            revert TransferFailed(
+                msg.sender,
+                listedNFT.seller,
+                listedNFT.price
+            );
 
-        nft.safeTransferFrom(owner, msg.sender, tokenId);
-        emit NFTSellSuccess(owner, msg.sender, tokenId);
+        nft.safeTransferFrom(listedNFT.seller, msg.sender, tokenId);
+        emit NFTSellSuccess(listedNFT.seller, msg.sender, tokenId);
     }
 
     /**
@@ -178,16 +153,46 @@ contract NFTMarket is EIP712 {
      */
     function checkWhiteList(
         address buyer,
-        uint deadline,
         SignedData calldata data
     ) private view {
         // check whether the signature is expired
-        if (block.timestamp > deadline) revert ExpiredSignature(deadline);
+        if (block.timestamp > data.deadline)
+            revert ExpiredSignature(data.deadline);
 
         bytes32 digest = hashStruct(
-            WhiteList({buyer: buyer, deadline: deadline})
+            WhiteList({buyer: buyer, deadline: data.deadline})
         );
         address signer = ecrecover(digest, data.v, data.r, data.s);
-        if (signer != owner) revert InvalidBuyer(signer, owner);
+        if (signer != owner()) revert InvalidSigner(signer, owner());
     }
+
+    error IsNotTheOwner(address owner, address seller);
+
+    error IsSelledOrNotListed(address nftAddr, uint tokenId);
+
+    error CannotBuyYouOwnNFT(address nftAddr, uint tokenId, address seller);
+
+    error TransferFailed(address from, address to, uint value);
+
+    error ExpiredSignature(uint256 deadline);
+
+    error InvalidSigner(address signer, address owner);
+
+    error InsufficientBalance(address buyer);
+
+    // Listed event
+    event Listed(
+        address indexed seller,
+        address indexed nftAddr,
+        uint indexed tokenId,
+        address tokenAddr,
+        uint price
+    );
+
+    // sell success event
+    event NFTSellSuccess(
+        address indexed seller,
+        address indexed buyer,
+        uint tokenId
+    );
 }
--- week3_day2/sign_and_verify/test/NFTMarket.t.sol
+++ week3_day2/sign_and_verify/test/NFTMarket.t.sol
@@ -20,30 +20,34 @@ contract NFTMarketTest is Test {
     );
 
     function setUp() public {
-        owner = makeAccount("owner");
         petToken = new PetToken();
         petNFT = new PetNFT();
-        nftMarket = new NFTMarket(address(petToken), owner.addr);
+        owner = makeAccount("owner");
+        vm.prank(owner.addr);
+        nftMarket = new NFTMarket();
     }
 
     function test_PermitBuy() public {
-        petNFT.mint(owner.addr, 1);
+        Account memory seller = makeAccount("seller");
+        petNFT.mint(seller.addr, 1);
         uint price = 1 ether;
         // listed
-        vm.prank(owner.addr);
-        nftMarket.list(address(petNFT), 1, price);
+        vm.prank(seller.addr);
+        nftMarket.list(address(petNFT), 1, address(petToken), price);
         // approve nft to nft market
-        vm.prank(owner.addr);
+        vm.prank(seller.addr);
         petNFT.approve(address(nftMarket), 1);
         // create the buyer's account
         Account memory alex = makeAccount("alex");
         deal(address(petToken), alex.addr, price);
-        // owner sign then add in whitelist
+        // seller sign then add in whitelist
         bytes32 digest = nftMarket.hashStruct(
             NFTMarket.WhiteList({buyer: alex.addr, deadline: 7 days})
         );
+        // nft market owner sign
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner.key, digest);
         NFTMarket.SignedData memory nftData = NFTMarket.SignedData({
+            deadline: 7 days,
             v: v,
             r: r,
             s: s
@@ -60,21 +64,15 @@ contract NFTMarketTest is Test {
         );
         (uint8 v1, bytes32 r1, bytes32 s1) = vm.sign(alex.key, digest1);
         NFTMarket.SignedData memory tokenData = NFTMarket.SignedData({
+            deadline: 1 days,
             v: v1,
             r: r1,
             s: s1
         });
         // except event
         vm.expectEmit(true, true, false, true);
-        emit NFTSellSuccess(owner.addr, alex.addr, 1);
+        emit NFTSellSuccess(seller.addr, alex.addr, 1);
         vm.prank(alex.addr);
-        nftMarket.permitBuy(
-            address(petNFT),
-            1,
-            7 days,
-            1 days,
-            nftData,
-            tokenData
-        );
+        nftMarket.permitBuy(address(petNFT), 1, nftData, tokenData);
     }
 }
